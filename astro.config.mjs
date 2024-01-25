07a425569bf1d77ebca0e3eba4c96d0a058ecfeb
Teodor Pytka <teodorpytka@gmail.com>
2024-01-25 16:13:47 +0100

    setup of deployment for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
+import netlify from '@astrojs/netlify'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -8,4 +10,6 @@ export default defineConfig({
       injectReset: true,
     }),
   ],
+  output: 'server',
+  adapter: netlify(),
 })
